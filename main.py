764f214317b7df0efbf3482593afa7e6b181d950
Matthew Garry <mhgarry92@gmail.com>
2023-10-24 18:06:32 -0400

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame # import library pygame to make our game
+import sys # import sys to use exit function 
+import os # import os to use path function and get the path of our game
+
+'''
+Variables
+'''
+# Game variables here
+
+'''
+Objects
+'''
+
+# Classes and functions here
+
+'''
+Setup
+'''
+
+# Run once code here
+
+'''
+Main Loop
+'''
+
+# Run every frame code here
