5812f95ed2009d45ab2e88431b1e08e8a06bd3e1
Matthew Garry <mhgarry92@gmail.com>
2023-10-24 18:14:14 -0400

    make game display variables

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import os # import os to use path function and get the path of our game
 Variables
 '''
 # Game variables here
+world_x = 960 # width of the game display
+world_y = 720 # height of the game display
+
+fps = 40 # frames per second of the game
+ani = 4 # animation cycles of the game (used to animate the sprites)
+
+world = pygame.display.set_mode([world_x,world_y]) # create the game display
+backdrop = pygame.image.load(os.path.join('pictures', 'mountain_background.svg')) # load the background image
+backdropbox = world.get_rect() # get the dimensions of the game display
 
 '''
 Objects
@@ -17,6 +26,8 @@ Objects
 Setup
 '''
 
+clock = pygame.time.Clock() # create a clock object to control time in our game
+pygame.init() # initialize pygame
 # Run once code here
 
 '''
